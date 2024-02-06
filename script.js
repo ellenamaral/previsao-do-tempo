5d44a8773e165e139dac12b424b51a1cc98d51e7
Ellen Amaral <ellenvatoz@gmail.com>
2024-02-06 20:45:07 -0300

    atualizei JS

--- script.js
+++ script.js
@@ -1,31 +1,20 @@
-
-
-const key= "137e0f6eca6480122109ed23692c0fa1"
-
-
+const key = "137e0f6eca6480122109ed23692c0fa1"
 
 function cliqueNoBotao() {
     const cidade = document.querySelector(".input-cidade").value
-
-    buscarCidade (cidade)
-
+    buscarCidade(cidade)
 }
 
-
-function colocarDadosNaTela (dados) {
-    console.log (dados)
+function colocarDadosNaTela(dados) {
+    console.log(dados)
     document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name
-    document.querySelector(".temperatura").innerHTML = Math.floor (dados.main.temp) + "°C"
+    document.querySelector(".temperatura").innerHTML = Math.floor(dados.main.temp) + "°C"
     document.querySelector(".clima").innerHTML = dados.weather[0].description
-    document.querySelector (".umidade").innerHTML = "Umidade: " + dados.main.humidity + "%"
-    document.querySelector (".icon-previsao").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
+    document.querySelector(".umidade").innerHTML = "Umidade: " + dados.main.humidity + "%"
+    document.querySelector(".icon-previsao").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
 }
 
-
-
-async function buscarCidade (cidade) {
-
-    const dados = await fetch (`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then(resposta => resposta.json())
-    
-    colocarDadosNaTela (dados)
+async function buscarCidade(cidade) {
+    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then(resposta => resposta.json())
+    colocarDadosNaTela(dados)
 }
