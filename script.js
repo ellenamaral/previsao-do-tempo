61f02fa01c552e4e24b8486d911751a4c79e6ac9
Ellen Amaral <ellenvatoz@gmail.com>
2024-02-06 20:15:12 -0300

    Atualizei JS

--- script.js
+++ script.js
@@ -2,21 +2,29 @@
 
 const key= "137e0f6eca6480122109ed23692c0fa1"
 
+
+
+function cliqueNoBotao() {
+    const cidade = document.querySelector(".input-cidade").value
+
+    buscarCidade (cidade)
+
+}
+
+
 function colocarDadosNaTela (dados) {
-    document.querySelector(".cidade").innerHTML = dados.name
+    console.log (dados)
+    document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name
+    document.querySelector(".temperatura").innerHTML = Math.floor (dados.main.temp) + "°C"
+    document.querySelector(".clima").innerHTML = dados.weather[0].description
+    document.querySelector (".umidade").innerHTML = "Umidade: " + dados.main.humidity + "%"
 }
 
 
-async function buscarCidade(cidade) {
 
-    const dados = await fetch (`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${API key}&lang=pt_br`).then (resposta => resposta.json())
+async function buscarCidade (cidade) {
+
+    const dados = await fetch (`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then(resposta => resposta.json())
     
-    console.log(dados);
-
-}
-
-function cliqueBotao () {
-    const cidade = document.querySelector (".input-cidade").value
-
-    buscarCidade(cidade)
+    colocarDadosNaTela (dados)
 }
